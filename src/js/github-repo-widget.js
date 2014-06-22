c000eecb7dedf6ce69ef1b03ac6b75e8ee2a6e04
Eymen Gunay <eymen@egunay.com>
2014-06-23 00:52:35 +0200

    GitHub widget responsive bugs fixed

--- src/js/github-repo-widget.js
+++ src/js/github-repo-widget.js
@@ -26,13 +26,6 @@
             +'.github-box .github-box-download .updated strong{font-weight:bold;color:#000}'
             +'.github-box .github-box-download .download{float:right;position:absolute;top:10px;right:10px;height:24px;line-height:24px;font-size:12px;color:#666;font-weight:bold;text-shadow:0 1px 0 rgba(255,255,255,0.9);padding:0 10px;border:1px solid #ddd;border-bottom-color:#bbb;border-radius:3px;background:#f5f5f5;background:-moz-linear-gradient(#f5f5f5,#e5e5e5);background:-webkit-linear-gradient(#f5f5f5,#e5e5e5);}'
             +'.github-box .github-box-download .download:hover{color:#527894;border-color:#cfe3ed;border-bottom-color:#9fc7db;background:#f1f7fa;background:-moz-linear-gradient(#f1f7fa,#dbeaf1);background:-webkit-linear-gradient(#f1f7fa,#dbeaf1);}'
-            +'@media (max-width: 767px) {'
-            +'.github-box .github-box-title{height:auto;min-height:60px}'
-            +'.github-box .github-box-title h3 .repo{display:block}'
-            +'.github-box .github-box-title .github-stats a{display:block;clear:right;float:right;}'
-            +'.github-box .github-box-download{height:auto;min-height:46px;}'
-            +'.github-box .github-box-download .download{top:32px;}'
-            +'}'
             +'</style>'
         );
         i++;
